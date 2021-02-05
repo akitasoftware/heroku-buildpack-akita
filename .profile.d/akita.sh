3e496651441d20ca3c4f511e3b9164b28424ef4c
Kevin Ku <kevin@akitasoftware.com>
2021-02-05 14:17:43 -0800

    Use HEROKU_SLUG_COMMIT in the trace name.

--- .profile.d/akita.sh
+++ .profile.d/akita.sh
@@ -2,22 +2,30 @@
 
 if [ -z "${AKITA_API_KEY_ID}" ]; then
   echo "AKITA_API_KEY_ID environment variable not set"
-  exit 0
+  exit 1
 fi
 
 if [ -z "${AKITA_API_KEY_SECRET}" ]; then
   echo "AKITA_API_KEY_SECRET environment variable not set"
-  exit 0
+  exit 1
 fi
 
 if [ -z "${AKITA_SERVICE}" ]; then
   echo "AKITA_SERVICE environment variable not set"
-  exit 0
+  exit 1
+fi
+
+if [ -z "${HEROKU_SLUG_COMMIT}" ]; then
+  echo "Heroku Labs Dyno Metadata not enabled"
+  exit 1
 fi
 
 if [ -z "${PORT}" ]; then
-  akita learn --service ${AKITA_SERVICE}
+  akita apidump \
+    --out akita://${AKITA_SERVICE}:trace:heroku-${HEROKU_SLUG_COMMIT}
 else
-  akita learn --service ${AKITA_SERVICE} --filter "port ${PORT}"
+  akita apidump \
+    --out akita://${AKITA_SERVICE}:trace:heroku-${HEROKU_SLUG_COMMIT} \
+    --filter "port ${PORT}"
 fi
 
